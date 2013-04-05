4e2b4152a0099f1ded959c9ee5b3d4d91d233094
cxrlospxndo <cxrlospxndo@gmail.com>
2013-04-04 23:16:31 -0500

    redis

--- app.rb
+++ app.rb
@@ -1,5 +1,29 @@
 require 'sinatra'
 
+redis = nil
+
+configure do
+	require 'redis'
+	dns = "pub-redis-11457.us-east-1-1.2.ec2.garantiadata.com"
+	port = "11457"
+	redis = Redis.new(:host => dns, :port => port)
+end
+
 get '/' do 
-	"Hello World"
+	d = redis.get 'day'
+	@message = "Acorta una url"
+	erb :new
+end
+
+post '/' do
+	@message = "Url acortada"
+	@shortened = params[:key]
+	@url = params[:url]
+	redis.set 'urls:' + @shortened, params[:url]
+	erb :new
+end
+
+get '/:shortened' do
+	url = redis.get 'urls:' + params[:shortened]
+	redirect url
 end
